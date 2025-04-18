0713755d0329eaba829c17725158b7b26420f983
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-04-18 12:06:28 +0600

    code hub

--- py_code_hub/reader/barcode_custom_svg.py
+++ py_code_hub/reader/barcode_custom_svg.py
@@ -9,14 +9,16 @@ class CustomSVGWriter(SVGWriter):
 # Custom writer options
 options = {
     'module_width': 0.2,  # Width of each barcode module
-    'module_height': 15.0,  # Height of each barcode module
-    'quiet_zone': 1.0,  # Quiet zone around the barcode
-    'font_size': 6,  # Size of the text
-    'text_distance': 5.0,  # Distance between barcode and text
+    'module_height': 8.0,  # Height of each barcode module
+    'quiet_zone': 0.5,  # Quiet zone around the barcode
+    'font_size': 4.5,  # Size of the text
+    'text_distance': 2.5,  # Distance between barcode and text
     'background': 'white',  # Background color
     'foreground': 'black',  # Foreground color (bars and text)
     'center_text': True,  # Center the text below barcode
-    'write_text': True
+    'write_text': True,
+    'margin_bottom': 0,
+    'margin_top': 10,
 }
 
 # Create barcode with custom options
