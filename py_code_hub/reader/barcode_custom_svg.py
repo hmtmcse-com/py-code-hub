15c3c39083b00ed424942669e2c46acc55e40122
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-04-18 14:18:25 +0600

    code hub

--- py_code_hub/reader/barcode_custom_svg.py
+++ py_code_hub/reader/barcode_custom_svg.py
@@ -3,12 +3,22 @@ from barcode.writer import SVGWriter
 
 
 class CustomSVGWriter(SVGWriter):
-    pass
+    top_text = None
+
+    def _init(self, code):
+        print(self.top_text)
+        super()._init(code)
+
+    # def _create_module(self, xpos, ypos, width, color):
+    #     print(f"Create Module {xpos}")
+    #
+    # def _create_text(self, xpos, ypos):
+    #     print("Create Text")
 
 
 # Custom writer options
 options = {
-    'module_width': 0.2,  # Width of each barcode module
+    'module_width': 0.4,  # Width of each barcode module
     'module_height': 8.0,  # Height of each barcode module
     'quiet_zone': 0.5,  # Quiet zone around the barcode
     'font_size': 4.5,  # Size of the text
@@ -17,8 +27,10 @@ options = {
     'foreground': 'black',  # Foreground color (bars and text)
     'center_text': True,  # Center the text below barcode
     'write_text': True,
-    'margin_bottom': 0,
+    'margin_bottom': 50,
     'margin_top': 10,
+    'human': "Human Readable Text \n abc \n cef",
+    'top_text': "My Top Text",
 }
 
 # Create barcode with custom options
