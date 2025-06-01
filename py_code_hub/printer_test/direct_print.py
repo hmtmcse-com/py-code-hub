aea158ffed5a7ef641bf7b9a0a2df808e88f5d9c
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-06-01 20:27:58 +0600

    Working on Printer

--- py_code_hub/printer_test/direct_print.py
+++ py_code_hub/printer_test/direct_print.py
@@ -2,7 +2,7 @@ from escpos.printer import Usb
 
 
 def print_document():
-    p = Usb(0x4b43, 0x3538, 0, out_ep=0x02)  # (vendor_id, product_id, interface, in_ep, out_ep)
+    p = Usb(0x4b43, 0x3538, 0, out_ep=0x03)  # (vendor_id, product_id, interface, in_ep, out_ep)
     p.set(align='center', font='a', width=2, height=2)
     p.text("Hello from HS-K24!\n")
     p.set(align='left', font='a', width=1, height=1)
