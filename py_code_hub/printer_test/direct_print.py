b9774a1b433727402fc1be9f9391b1928800d076
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-06-01 19:35:53 +0600

    Added Readme

--- py_code_hub/printer_test/direct_print.py
+++ py_code_hub/printer_test/direct_print.py
@@ -1,7 +1,7 @@
 from escpos.printer import Usb
 
 
-def print():
+def print_document():
     printer = Usb(0x4b43, 0x3538, 0, 0x81, 0x03)
     printer.text('\x1b\x40')
     printer.text('\x1b\x64\x00')
@@ -9,4 +9,4 @@ def print():
     printer.cut('\x1d\x64')
 
 
-print()
+print_document()
