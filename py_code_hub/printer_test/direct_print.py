a8d8e593fb0eba923c992bbd827c2320ad1fa064
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-06-01 19:44:48 +0600

    Added Readme

--- py_code_hub/printer_test/direct_print.py
+++ py_code_hub/printer_test/direct_print.py
@@ -2,11 +2,12 @@ from escpos.printer import Usb
 
 
 def print_document():
-    printer = Usb(0x4b43, 0x3538, 0, 0x81, 0x03)
-    # printer.text('\x1b\x40')
-    # printer.text('\x1b\x64\x00')
-    printer.text("\n" + "This is test print" + "\n")
-    printer.cut('\x1d\x64')
+    p = Usb(0x4b43, 0x3538, 0, 0x81, 0x02)  # (vendor_id, product_id, interface, in_ep, out_ep)
+    p.set(align='center', font='a', width=2, height=2)
+    p.text("Hello from HS-K24!\n")
+    p.set(align='left', font='a', width=1, height=1)
+    p.text("Thank you for your purchase!\n")
+    p.cut()
 
 
 print_document()
